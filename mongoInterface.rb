29b15dc4e2ac4081433afd8f235912b04be34c6e
olivier <fetcher@f-olivier.(none)>
2012-11-28 18:56:20 -0300

    first commit

--- mongoInterface.rb
+++ mongoInterface.rb
@@ -8,73 +8,71 @@ require 'rack-flash'
 
 class MongoInterface < Sinatra::Base
 
-include Mongo
-
 set :haml, :format => :html5
 use Rack::Flash
 enable :sessions
 
-get '/style.css' do
-	sass :style
-end
+	get '/style.css' do
+		sass :style
+	end
 
-get '/' do
-	@flash = flash[:notice]
-  haml :index
-end
+	get '/' do
+		@flash = flash[:notice]
+	  haml :index
+	end
 
-post '/resultat' do
-	session[:filter] = params[:filter]
-  session[:streamType] = params[:streamType]
-  session[:streamArgument] = params[:streamArgument]
-  session[:login] = params[:login]
+	post '/resultat' do
+		session[:filter] = params[:filter]
+	  session[:streamType] = params[:streamType]
+	  session[:streamArgument] = params[:streamArgument]
+	  session[:login] = params[:login]
 
-  redirect '/resultat'
-end
+	  redirect '/resultat'
+	end
 
-get '/resultat' do
-	
-	@client =  client
-	@db = db
-	@columnsCollection = columnsCollection
-	@usersCollection = usersCollection
+	get '/resultat' do
+		
+		@client =  client
+		@db = db
+		@columnsCollection = columnsCollection
+		@usersCollection = usersCollection
 
-	filter = session[:filter] 
-  streamType = session[:streamType]
-  streamArgument = session[:streamArgument].split
-  login = session[:login] 
-  
- to_insert_to_columns = { 
- 	"filter"=>
-  [{"type"=>"text",
-    "property"=>"articleBody",
-    "operator"=>"includes",
-    "value"=>[filter]}],
- "source"=>
-  [{"streamType"=> streamType,
-    "streamArgument"=> streamArgument,
-    "provider"=>"twitter",
-    "endpoint"=>"user",
-    "viewer"=>308762265.0}] 
-  }
+		filter = session[:filter] 
+	  streamType = session[:streamType]
+	  streamArgument = session[:streamArgument].split
+	  login = session[:login] 
+	  
+	 to_insert_to_columns = { 
+	 	"filter"=>
+	  [{"type"=>"text",
+	    "property"=>"articleBody",
+	    "operator"=>"includes",
+	    "value"=>[filter]}],
+	 "source"=>
+	  [{"streamType"=> streamType,
+	    "streamArgument"=> streamArgument,
+	    "provider"=>"twitter",
+	    "endpoint"=>"user",
+	    "viewer"=>308762265.0}] 
+	  }
 
-  @columnsCollection.insert(to_insert_to_columns)
-  column_id = @columnsCollection.find.sort(:_id => :desc ).limit(1).find.each {|i| p i} ['_id']
-  
-  to_insert_to_users = { 
-  "columns"=>[column_id],
-  "login"=>login 
-	}
+	  @columnsCollection.insert(to_insert_to_columns)
+	  column_id = @columnsCollection.find.sort(:_id => :desc ).limit(1).find.each {|i| p i} ['_id']
+	  
+	  to_insert_to_users = { 
+	  "columns"=>[column_id],
+	  "login"=>login 
+		}
 
-	@usersCollection.insert(to_insert_to_users)
+		@usersCollection.insert(to_insert_to_users)
 
-	user_id = @usersCollection.find.sort(:_id => :desc ).limit(1).find.each {|i| p i} ['_id']
+		user_id = @usersCollection.find.sort(:_id => :desc ).limit(1).find.each {|i| p i} ['_id']
 
-	flash[:notice] = "Here is the user_id from users collection to be inserted in shore : #{user_id}"
+		flash[:notice] = "Here is the user_id from users collection to be inserted in shore : #{user_id}"
 
-  #binding.pry
-  redirect '/'
-end
+	  #binding.pry
+	  redirect '/'
+	end
 
 helpers do
 	def client
